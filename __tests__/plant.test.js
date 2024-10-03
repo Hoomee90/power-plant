2084b745dd55e32f4c763b76611919b99970e029
Samantha Callie <Hoomee90@gmail.com>
2024-10-03 13:19:01 -0700

    add function factor and state storage

---
+++ __tests__/plant.test.js
@@ -0,0 +1,30 @@
+import { storeState, changeState } from '../src/js/plant.js';
+
+describe('storeState', () => {
+  let stateControlTest;
+
+  beforeEach(() => {
+    stateControlTest = storeState();
+  });
+
+  test('is defined', () => {
+    expect(storeState).toBeDefined();
+  });
+
+  test('returns empty state object', () => {
+
+    expect(stateControlTest()).toEqual({});
+  });
+});
+
+describe('changeState', () => {
+  let stateControlTest;
+
+  beforeEach(() => {
+    stateControlTest = storeState();
+  });
+
+  test('returns correct functions', () => {
+    expect(changeState("water")(3)({})).toEqual({ water: 3 });
+  });
+});
---
+++ src/js/plant.js
@@ -0,0 +1,17 @@
+export const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  }
+}
+
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: (state[prop] || 0) + value
+    })
+  }
+}
