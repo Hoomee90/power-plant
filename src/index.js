f8bae78617429f24d7b8e8bd78fca10e532bd37e
Samantha Callie <Hoomee90@gmail.com>
2024-10-08 13:15:37 -0700

    add ui logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import './scss/styles.scss';
 import 'bootstrap';
+import { stateControl, changeStateFunctions } from './js/utils.js'
+
+window.onload = () => {
+  document.querySelector('#feed').onclick = () => {
+    const newState = stateControl(changeStateFunctions.feed);
+    document.querySelector('#soil-value').innerText = `${newState.soil}`;
+  };
+  document.querySelector('#water').onclick = () => {
+    const newState = stateControl(changeStateFunctions.water);
+    document.querySelector('#water-value').innerText = `${newState.water}`;
+  };
+  document.querySelector('#spotlight').onclick = () => {
+    const newState = stateControl(changeStateFunctions.spotlight);
+    document.querySelector('#light-value').innerText = `${newState.light}`;
+  };
+
+};
